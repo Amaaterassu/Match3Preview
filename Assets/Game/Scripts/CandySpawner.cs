af484f3a54ae1b299ce7057c574083881a729e56
RomanKapustinskyi <kapustinskyiroman@gmail.com>
2022-08-28 08:43:42 +0300

    refactor

--- Assets/Game/Scripts/CandySpawner.cs
+++ Assets/Game/Scripts/CandySpawner.cs
@@ -3,40 +3,47 @@ using System.Collections.Generic;
 
 public class CandySpawner : MonoBehaviour
 {
-    FieldGenerator fieldGenerator;
-    public GameObject theGameObjectWithCode;
+    public FieldGenerator fieldGenerator;
     [Space]
     public Transform parentGameObject;
     [Space]
     public bool SpawnOnStart;
-   
+
+    private List<GameObject> _candiesVariants;
+
     private void Start()
     {
+        _candiesVariants = new List<GameObject>(Resources.LoadAll<GameObject>("CandiesPrefabs"));
+
         if (SpawnOnStart)
         {
             SpawnRandomCandy();
         }
     }
 
-    private List<GameObject> candiesList;
     public void SpawnRandomCandy()
     {
+        if (!fieldGenerator) // analog of fieldGenerator == null
+        {
+            Debug.LogError($"Field generator is missing at gameobject with name: {name}"); // more appropriate error log format
+            return;
+        }
         if (parentGameObject == null)
         {
             Debug.LogError("You will have no candies until you asign the Parent Game Object in the CandySpawner script!!!");
-                return;
+            return;
         }
-        fieldGenerator = theGameObjectWithCode.GetComponent<FieldGenerator>();
-        float zVectorOfFieldPositionMinus1 = theGameObjectWithCode.GetComponent<Transform>().position.z -1;
-        candiesList = new List<GameObject>(Resources.LoadAll<GameObject>("CandiesPrefabs"));
+
+        float zVectorOfFieldPositionMinus1 = fieldGenerator.transform.position.z - 1;
+
         for (int col = 0; col < fieldGenerator.columnCount; col++)
         {
             for (int row = 0; row < fieldGenerator.rowCount; row++)
             {
-                int randomCandy = Random.Range(0, (candiesList.Count - 1));
-                GameObject instanceSpawn = Instantiate(candiesList[randomCandy], parentGameObject);
+                int randomCandy = Random.Range(0, _candiesVariants.Count);
+                GameObject instanceSpawn = Instantiate(_candiesVariants[randomCandy], parentGameObject);
                 instanceSpawn.transform.localPosition = new Vector3(col * fieldGenerator.spawnHeightSpace, row * fieldGenerator.spawnWidthSpace, zVectorOfFieldPositionMinus1);
-                
+
             }
         }
     }
